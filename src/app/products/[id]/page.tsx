ce518b6899ac4c7a5c9a1b9e2d5bc4a7deb5b3c2
Your Name <youremail@example.com>
2023-11-15 18:55:11 +0200

    boxes and products pages

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import CarCard from "@/components/CarCard";
-import fetchCars from "@/utils";
 
 interface IProps {}
 
 const page = async ({}: IProps) => {
-  const allProducts = await fetchCars();
   return (
     <div className=" container mx-auto">
       <h2>welcome</h2>
