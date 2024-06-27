4c4ca096cbade6dc53b7de50f37af77651fe8fb0
salem <salem.mohamed@cibeg.com>
2024-06-27 13:24:48 +0300

    fiwjoawfiopwfa

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,7 @@
-
-interface IProps {
-  
-}
+interface IProps {}
 
 const page = ({}: IProps) => {
-  return <div className=" flex justify-center items-center">About</div>
-}
+  return <div className=" flex justify-center items-center">About Amin</div>;
+};
 
-export default page
+export default page;
